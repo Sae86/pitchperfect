a24037d46306dacbece73cf53dd3656fa8d085f3
Sae86 <Sae@Saes-MacBook-Pro.local>
2015-09-08 12:37:51 -0500

    First commit
    All project files

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Sae on 8/16/15.
+//  Copyright (c) 2015 Sae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+ 
+    
+    @IBAction func recordStart(sender: UIButton) {
+        recordLabel.hidden = false
+        // TODO: record user's voice
+        println("yay!!")
+    }
+
+    @IBAction func recordStop(sender: UIButton) {
+        recordLabel.hidden = true
+        // TODO: stop recording user's voice
+        println("uuh!!")
+    }
+}
+
