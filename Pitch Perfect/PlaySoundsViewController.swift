bf20e57c43b9c2a5a7d0ba31da4dd1672cebf249
Sae86 <Sae@Saes-MacBook-Pro.local>
2015-09-14 17:44:51 -0500

    slow button slows and plays mp3

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -7,13 +7,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
+            var filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+            audioPlayer.enableRate = true
+        }
+        else{
+            println("The file path is empty")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,4 +30,9 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playSlowAudio(sender: UIButton) {
+        // play audio slowly
+        audioPlayer.rate = 0.5
+        audioPlayer.play()
+    }
 }
