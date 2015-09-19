1f0373f950f6268939b49551e4d7d8487811a536
Sae86 <Sae@Saes-MacBook-Pro.local>
2015-09-19 09:57:57 -0500

    Removing unused functions

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -24,11 +24,6 @@ class PlaySoundsViewController: UIViewController {
         audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func playSlowAudio(sender: UIButton) {
         // play audio slowly
         playAudioWithVariableRate(0.5)
